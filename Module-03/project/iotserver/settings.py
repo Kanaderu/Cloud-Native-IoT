228c27b93e880cfd78fd4c44deddae96e50a4a1e
David Fan <fand.kanade@gmail.com>
2025-06-26 17:59:50 -0500

    feat: adds celery to django

--- Module-03/project/iotserver/settings.py
+++ Module-03/project/iotserver/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_filters',
+    'rest_framework',
+    # 'rest_framework_simplejwt',
+    'graphene_django',
+    'oauth2_provider',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
